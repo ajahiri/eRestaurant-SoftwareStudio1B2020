bb39b0f4fffbc5f0de9fd9ffe2457035aac652f2
younesDev00 <13768282@student.uts.edu.au>
2020-05-05 21:14:33 +1000

    Added Change Password Option

--- client/ui/components/useraccount.js
+++ client/ui/components/useraccount.js
@@ -4,25 +4,28 @@ import {MDBContainer, MDBRow, MDBCol, MDBInput, MDBTypography, MDBBtn,} from  "m
 import { Accounts } from 'meteor/accounts-base';
 
 
-var namechanged = false, addresschanged = false, emailchanged = false, phonechanged = false, changepassword = false ;
+var namechanged = false, phonechanged = false, changepassword = false ;
 class UserAcount extends React.Component {
     constructor(props) {
         super(props);
         //state
-        this.state = {fullname: '', email: "",address: "", phone: "",oldpassword: "", newpassword: ""};
+        this.state = {fullname: '', passwordlabel: "Please Choose New Password", phone: "",oldpassword: "", newpassword: ""};
         //methods
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
 
         }
         //s
+
+
+
         handleChange(event) 
         {
             const target = event.target;
             console.log(target.name);
             console.log(target.value);
 
-            const value = target.name === 'abdul' ? target.checked : target.value;
+            const value = target.name === 'WhiteOne' ? target.checked : target.value;
             const name = target.name;
 
             this.setState({
@@ -33,6 +36,59 @@ class UserAcount extends React.Component {
                 namechanged = true;
             if(name == 'phone')
                 phonechanged = true;
+            if(name == 'phone')
+                phonechanged = true;
+
+            if(name == "newpassword")
+            {
+                changepassword = true;
+            }
+
+            // if(name == 'oldpassword' || name == 'newpassword')       
+            // {
+            //     if(this.state.oldpassword != this.state.newpassword)
+            //     {
+            //         if(this.state.newpassword != null && this.state.newpassword !="" && this.state.newpassword != " ")
+            //         {
+            //             let str = this.state.newpassword;
+            //             if(str.length > 8)
+            //             {
+            //                 if(this.state.oldpassword != this.state.newpassword)
+            //                 {
+            //                     this.setState({
+            //                         passwordlabel: ""
+            //                     });
+            //                     changepassword = true;
+            //                 }else{
+            //                     this.setState({
+            //                         passwordlabel: "Please Choose Diffrent New Password as it matches with old one"
+            //                     });
+            //                 }
+                            
+            //             }else{
+            //                 this.setState({
+            //                     passwordlabel: "New Password must be more than 8 charachters"
+            //                 });
+            //             }
+            //         }else{
+            //             this.setState({
+            //                 passwordlabel: "New Password is Empty"
+            //             });
+            //         }
+            //     }else{
+            //         if(this.state.oldpassword != null && this.state.oldpassword !="" && this.state.oldpassword != " ")
+            //         {
+            //             this.setState({
+            //                 passwordlabel: "Please Choose Diffrent New Password as it matches with old one"
+            //             });
+            //         }else{
+            //             this.setState({
+            //                 passwordlabel: ""
+            //             });
+            //         }
+
+            //     }
+            // }
 
         }
         
@@ -48,14 +104,33 @@ class UserAcount extends React.Component {
                 Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.phone": this.state.phone}});
                 phonechanged = false;
             }
+            console.log(changepassword);
+            console.log(this.state.oldpassword);
+            console.log(this.state.newpassword);
             if(changepassword)
             {
-                    Accounts.changePassword(oldPassword, newPassword, [callback])
-                    changepassword = false;
+                Accounts.changePassword(this.state.oldpassword, this.state.newpassword, function (e, r) {
+                    if (e) {
+                        console.log(e.reason);
+                    } else {
+                        // success
+                    }
+                });
+                changepassword = false;
             }
             
             event.preventDefault();
           }
+          forgetPassword = () => {
+            let email = this.refs.email.value;
+            Accounts.forgotPassword({email: email}, function (e, r) {
+                if (e) {
+                    console.log(e.reason);
+                } else {
+                    // success
+                }
+            }); 
+        }
           
 
           //this.setState({name: Meteor.user().profile.name});
@@ -97,19 +172,25 @@ class UserAcount extends React.Component {
                     </MDBCol>
                 </MDBCol>
             </MDBRow>
+
             <MDBRow left>
                 <MDBCol  className="form-inline" sm="12">
-                    <MDBCol sm="6" md="3">
-                    <span className="font-weight-bold"><strong className="badge badge-primary text-wrap ">Phone: </strong> {Meteor.user().profile.phone}</span>
+                    <MDBCol sm="6" md="4">
+                    <span className="font-weight-bold"><strong className="badge badge-danger text-wrap ">Change Password: {this.state.oldpassword} {this.state.newpassword} </strong></span>
                     </MDBCol>
-                    <MDBCol sm="6" md="3">
-                        <MDBInput label="PhoneNumber" size="lg" name='phone' type="number" onChange={this.handleChange} />
+                    <MDBCol  sm="6" md="4">
+                        <MDBInput label="Old Password" size="lg" name='oldpassword' type="password" onChange={this.handleChange} />
                     </MDBCol>
-                    <MDBCol sm="6" md="3">
-                        {phonechanged ? <span>Change Phone Number to {this.state.phone}</span> : <span></span>}
+                    <MDBCol  sm="6" md="4">
+                        <MDBInput label="New Password" size="lg" name='newpassword' type="password" onChange={this.handleChange} />
+                    </MDBCol>
+                    <MDBCol sm="6" md="12">
+                        <span>{this.state.passwordlabel}</span>
                     </MDBCol>
                 </MDBCol>
             </MDBRow>
+
+
             <MDBRow center>
                 <MDBRow className='btn-confirm-padding'>
                         <MDBCol><MDBBtn onClick={this.handleSubmit}>Confirm Change</MDBBtn></MDBCol>
