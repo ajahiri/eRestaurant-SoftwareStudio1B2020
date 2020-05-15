22db5376f9c14369b6280001235a218d02a0cef7
Anthony Vu <53590640+Thouny@users.noreply.github.com>
2020-05-15 23:50:52 +1000

    Added small printer icon to button

--- client/ui/pages/booking/booking_summary.js
+++ client/ui/pages/booking/booking_summary.js
@@ -1,11 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import {MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage,
   MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn, MDBCardGroup, MDBView, MDBMask, MDBIcon} from  "mdbreact";
-
+import ic_print from "./ic_print_24px.png"
 
 class BookingSummary extends React.Component{
     constructor(props) {
@@ -16,7 +15,7 @@ class BookingSummary extends React.Component{
     render(){
         return(
           <MDBCol>
-          <MDBCard center style={{ width: "45rem" }}>  
+          <MDBCard style={{ width: "45rem" }}>  
             <MDBCardBody>
               <MDBCardTitle tag="h2" className="page-heading, font-weight-bold text-center mb-3 p-3">Booking Summary</MDBCardTitle>              
               <MDBRow>
@@ -31,17 +30,17 @@ class BookingSummary extends React.Component{
                             <h5 className="font-weight-bold mb-4 p-0">Order #123</h5>
                             <h5 className="font-weight-bold mb-4 p-0">Table for 2 on May 15, 2020 at 7:00pm</h5>
                             <h6 className=" mb-4 p-0">First Name Last Name</h6>
-                            
-                            
                             <hr />    
                             <h6>Sapori Unici Ashfield</h6>
                             <span> Address: Grimmond Ave, Ashfield NSW 2131</span>
                             <br></br>
-                            <span> Phone: 02 446 668</span>                            
+                            <span> Phone: 02 446 668</span>
+                           
                         </MDBCol>
-
               </MDBRow>
-              <MDBBtn href="#" > Print</MDBBtn>
+
+              <MDBBtn href="#"> <img src={ic_print}></img>Print</MDBBtn>
+            
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
@@ -49,5 +48,5 @@ class BookingSummary extends React.Component{
     }
 }
 
-
 export default BookingSummary;
+
